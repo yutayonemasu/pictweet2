2df2da97020d930f660b1088ad0de825ddc57588
yutayonemasu <yonemost777@yahoo.co.jp>
2016-06-02 15:35:37 +0900

    add routes tweets edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
- resources :tweets, only: [:index,:new,:create,:destroy]
+ resources :tweets, only: [:index,:new,:create,:destroy :edit]
  resources :users , only: [:show,]
  end
