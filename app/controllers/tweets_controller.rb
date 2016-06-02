3afe76306d31618ecde81284ff61ab8ab52c721f
yutayonemasu <yonemost777@yahoo.co.jp>
2016-06-02 18:29:37 +0900

    ツイート詳細機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,6 +22,24 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+      end
+    end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+
+  end
+
   private
   def tweet_params
     params.permit(:image, :text)
@@ -30,5 +48,4 @@ class TweetsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
 
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
- resources :tweets, only: [:index,:new,:create,:destroy :edit]
+ resources :tweets, only: [:index,:new,:create,:destroy,:edit,:update,:show]
  resources :users , only: [:show,]
  end
