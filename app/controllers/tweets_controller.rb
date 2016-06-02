2699eece18b599f1f86fa33660284d23eea1f5eb
yutayonemasu <yonemost777@yahoo.co.jp>
2016-06-02 12:55:53 +0900

    add tweets#create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,7 +12,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   private
@@ -23,5 +23,5 @@ class TweetsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
 
-
+end
 end
